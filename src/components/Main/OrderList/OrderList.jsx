87f9e94b932dd1a712edc10c2e4955ff89846007
Valerii Borisenko <valerii.borisenko@gmail.com>
2025-03-20 14:57:19 +0100

    add motion

--- src/components/Main/OrderList/OrderList.jsx
+++ src/components/Main/OrderList/OrderList.jsx
@@ -1,6 +1,8 @@
 import { Fragment } from "react";
 import "./order-list.css"
 import OrderItem from "./OrderItem/OrderItem";
+// eslint-disable-next-line no-unused-vars
+import { AnimatePresence, motion } from "motion/react";
 
 const OrderList = (props) => {
     const {theme, order, totalPrice, addOrder, removeOrder} = props
@@ -13,18 +15,26 @@ const OrderList = (props) => {
                 order.length === 0 ? <p className="fw-bold m-0 p-4">Your order is empty. Add something delicious!</p> : 
                 <Fragment>
                     <ul className="ordel-list__container list-unstyled d-flex flex-column m-0">
-                        {order.map((item, index) =>
-                            <li key={`orderMenu${index}`}>
-                                <OrderItem 
-                                    id={item?.id}
-                                    name={item?.name}
-                                    price={item?.price}
-                                    amount={item?.amount}
-                                    addOrder={addOrder}
-                                    removeOrder={removeOrder}
-                                />
-                            </li>
-                        )}
+                        <AnimatePresence>
+                            {order.map((item, index) =>
+                                <motion.li 
+                                    key={`orderMenu${index}`}
+                                    initial={{ opacity: 0, height: 0, margin: 0, padding: 0 }}
+                                    animate={{ opacity: 1, height: 'auto'}}
+                                    exit={{ opacity: 0, height: 0, margin: 0, padding: 0 }}
+                                    transition={{ duration: 0.3 }}
+                                >
+                                    <OrderItem 
+                                        id={item?.id}
+                                        name={item?.name}
+                                        price={item?.price}
+                                        amount={item?.amount}
+                                        addOrder={addOrder}
+                                        removeOrder={removeOrder}
+                                    />
+                                </motion.li>
+                            )}
+                        </AnimatePresence>
                     </ul>
                     <div className="d-flex flex-row justify-content-between align-items-center p-2 pt-3 pb-3">
                         <p className="fw-bold m-0">Total</p>
